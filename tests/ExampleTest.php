ea2a6bb3616b7944ea10cd304fae7b3bc4bd5695
Developer <Developer>
2016-12-18 16:17:48 +0100

    Test created

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -12,4 +12,29 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel');
     }
+
+    public function testImageUpload()
+    {
+        $stub = __DIR__ . '/test.jpeg';
+        $name = time() . '.jpeg';
+        $path = sys_get_temp_dir() . '/' . $name;
+
+        copy($stub, $path);
+
+        $file = new UploadedFile($path, $name, filesize($path), 'image/jpeg', null, true);
+
+        $file_path = 'storage/uploads/images/' . $name;
+
+        $this->json('POST', '/api/upload-image', ['image' => $file])
+             ->assertResponseOk()
+             ->seeJson([
+                 'errors' => false,
+                 'data' => [
+                     'path' => $file_path,
+                 ],
+             ])
+             ->assertFileIsReadable(public_path($file_path));
+
+        @unlink(public_path($file_path));
+    }
 }
